2343b6d9529bb350fb0a9ecb92dbcf2e9f418486
霍运浩 <372799514@qq.com>
2022-05-01 20:14:07 +0800

    代码

---
+++ main/src/store/leftNav.js
@@ -0,0 +1,38 @@
+import * as loading from './loading'
+
+import * as appInfo from '../store'
+
+export const navList = [
+  {
+    name: 'react15',// 唯一
+    entry: '//localhost:9002/', //host
+    loading, 
+    container: '#micro-container',  //渲染容器
+    activeRule: '/react15', //路由
+    appInfo, //
+  },
+  {
+    name: 'react16',
+    entry: '//localhost:9003/',
+    loading,
+    container: '#micro-container',
+    activeRule: '/react16',
+    appInfo,
+  },
+  {
+    name: 'vue2',
+    entry: '//localhost:9004/',
+    loading,
+    container: '#micro-container',
+    activeRule: '/vue2',
+    appInfo,
+  },
+  {
+    name: 'vue3',
+    entry: '//localhost:9005/',
+    loading,
+    container: '#micro-container',
+    activeRule: '/vue3',
+    appInfo,
+  },
+];
